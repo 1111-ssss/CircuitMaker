896421403b0bc7cec82a1f863a54da0f08feeec3
1111-ssss <gurskievgeni@gmail.com>
2026-08-24 21:57:19 +0300

    feature(circuit): update fields

--- server/src/DAL/Entities/Circuit.cs
+++ server/src/DAL/Entities/Circuit.cs
@@ -5,4 +5,8 @@ public class Circuit
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string CreatedBy { get; set; } = string.Empty;
+    public CircuitSettings Settings { get; set; } = new();
+    public List<CircuitNode> Nodes { get; set; } = new();
+    public List<CircuitEdge> Edges { get; set; } = new();
+    public Dictionary<Guid, User> ConnectedUsers { get; set; } = new();
 }
