fa814542fecac1a41390cc6d3e47570eb13dff0c
Zbett ! <zbett@192.168.100.10>
2026-08-24 21:48:49 +0300

    refactor(repository): rename methods in interface

--- server/src/DAL/Interfaces/ICircuitRepository.cs
+++ server/src/DAL/Interfaces/ICircuitRepository.cs
@@ -4,11 +4,11 @@ namespace DAL.Interfaces;
 
 public interface ICircuitRepository
 {
-    Circuit CreateCircuit(string name, string createdBy, CircuitSettings settings);
+    Circuit Create(string name, string createdBy, CircuitSettings settings);
     Circuit? GetById(Guid id);
     IEnumerable<Circuit> GetAll();
-    bool AddUserToCircuit(Guid circuitId, User user);
-    bool RemoveUserFromCircuit(Guid circuitId, Guid connectionId, out User? removedUser);
+    bool AddUser(Guid circuitId, User user);
+    bool RemoveUser(Guid circuitId, Guid connectionId, out User? removedUser);
     void UpdateNodes(Guid circuitId, List<CircuitNode> nodes);
     void UpdateEdges(Guid circuitId, List<CircuitEdge> edges);
 }
